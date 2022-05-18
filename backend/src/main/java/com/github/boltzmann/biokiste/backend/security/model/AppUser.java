b5554e30eb6726f610bfdd77f442a8bc21c2172c
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-18 15:36:48 +0200

    Add login_whenValidCredentials_thenReturnValidJWT so that no red flags are shown in Intellij. Test is red, much to do to get it green.

---
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/model/AppUser.java
@@ -0,0 +1,21 @@
+package com.github.boltzmann.biokiste.backend.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("appusers")
+@Builder
+public class AppUser {
+    @Id
+    private String id;
+    private String name;
+    private String customerId;
+    private String password;
+}
