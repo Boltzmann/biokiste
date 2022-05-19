8fa9641660538a1195260e606997b8c819a0294a
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-19 09:11:15 +0200

    Add authenticated user with login in backend. Test is green. However, local test by hand give 500 in this case.

--- backend/src/main/java/com/github/boltzmann/biokiste/backend/security/model/AppUser.java
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/model/AppUser.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class AppUser {
     @Id
     private String id;
-    private String name;
+    private String username;
     private String customerId;
     private String password;
 }
---
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/service/JWTUtilService.java
@@ -0,0 +1,35 @@
+package com.github.boltzmann.biokiste.backend.security.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+
+@Service
+public class JWTUtilService {
+    @Value("${piphi.biokisteapp.jwt.secret}")
+    private String JWT_SECRET;
+
+    public String createToken(String username){
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(username)
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(Duration.ofHours(6))))
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
