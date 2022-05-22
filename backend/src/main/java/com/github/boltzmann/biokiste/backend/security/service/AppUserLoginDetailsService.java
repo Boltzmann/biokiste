4fe867ae06265af29e86d4c6c338c31172691ae9
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-22 22:31:08 +0200

    Refactored: AppUserDetails is a model not a dto. AppUserRepository/Service/... -> AppUserLoginRepository/Service/..., to distinguish from AppUserDetails.

--- backend/src/main/java/com/github/boltzmann/biokiste/backend/security/service/AppUserLoginDetailsService.java
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/service/AppUserLoginDetailsService.java
@@ -1,8 +1,7 @@
 package com.github.boltzmann.biokiste.backend.security.service;
 
-import com.github.boltzmann.biokiste.backend.dto.AppUserDetails;
 import com.github.boltzmann.biokiste.backend.security.model.AppUser;
-import com.github.boltzmann.biokiste.backend.security.repository.AppUserRepository;
+import com.github.boltzmann.biokiste.backend.security.repository.AppUserLoginRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,21 +13,21 @@ import java.util.NoSuchElementException;
 @Service
 public class AppUserLoginDetailsService implements UserDetailsService {
 
-    private final AppUserRepository appUserRepository;
+    private final AppUserLoginRepository appUserLoginRepository;
 
-    public AppUserLoginDetailsService(AppUserRepository appUserRepository) {
-        this.appUserRepository = appUserRepository;
+    public AppUserLoginDetailsService(AppUserLoginRepository appUserLoginRepository) {
+        this.appUserLoginRepository = appUserLoginRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser appUser = appUserRepository.findByUsername(username)
+        AppUser appUser = appUserLoginRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Username not found."));
         return new User(appUser.getUsername(), appUser.getPassword(), List.of());
     }
 
     public AppUser getUserByName(String username){
-        return appUserRepository.findByUsername(username)
+        return appUserLoginRepository.findByUsername(username)
                 .orElseThrow( () -> new NoSuchElementException("User not found with name: " + username) );
     }
 
