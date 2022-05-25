e23e5f8cbef0c2b361e107376679c0d4ec52fbfd
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-25 13:25:33 +0200

    Get SSL working. Test is green. Use baeldung example keystore from https://www.baeldung.com/spring-boot-https-self-signed-certificate or better https://github.com/eugenp/tutorials/blob/master/spring-security-modules/spring-security-web-boot-2

--- backend/src/main/java/com/github/boltzmann/biokiste/backend/security/controller/WelcomeController.java
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/controller/WelcomeController.java
@@ -2,14 +2,16 @@ package com.github.boltzmann.biokiste.backend.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping
     public String welcome() {
-        return "ssl/welcome";
+        return "<h1>Welcome to Secured Site</h1>";
     }
 
 }
