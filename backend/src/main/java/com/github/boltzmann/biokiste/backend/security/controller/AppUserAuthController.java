7adfcbad2d09fac15b5ebf4429385f4ba93bc528
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-25 15:18:32 +0200

    Add CrossOrigin annotation to use Android app against Heroku. Now the app gets CORS errors.

--- backend/src/main/java/com/github/boltzmann/biokiste/backend/security/controller/AppUserAuthController.java
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/security/controller/AppUserAuthController.java
@@ -5,12 +5,10 @@ import com.github.boltzmann.biokiste.backend.security.service.JWTUtilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/auth")
 public class AppUserAuthController {
 
