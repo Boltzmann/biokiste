8b03d5ab327fe7713eb416ec02953b14966182f5
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-05-31 15:18:44 +0200

    Removed SonarCloud code smells.

--- web-frontend/src/components/AppHead.tsx
+++ web-frontend/src/components/AppHead.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./AppHead.css"
 import {useNavigate} from "react-router-dom";
-import useUserDetails from "../hooks/useUserDetails";
 
 export default function AppHead(){
     const navigate = useNavigate()
