a8c0decf1368fdf60c438d6c6ec43e3c006a927f
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-06-03 15:27:48 +0200

    User can add a subscription for herself using the frontend.

--- web-frontend/src/components/NewSubscription.tsx
+++ web-frontend/src/components/NewSubscription.tsx
@@ -1,13 +1,23 @@
 import {UserDetails} from "../model/UserDetails";
+import {SubscriptionOverviewDto} from "../dto/SubscriptionOverviewDto";
 
 type NewSubscriptionProps = {
     userDetails: UserDetails | undefined
     subscribeToBox: (boxId: string) => void
+    subscribables: SubscriptionOverviewDto[] | undefined
 }
 
-export default function NewSubscription({ userDetails, subscribeToBox }: NewSubscriptionProps ){
+export default function NewSubscription({ userDetails, subscribeToBox , subscribables}: NewSubscriptionProps ){
 
     return <div>
-        <button onClick={() => subscribeToBox("4711")}> Add {userDetails && userDetails.id} to Box 4711</button>
+        {subscribables &&
+            subscribables.map(
+                subsls =>
+                    <button onClick={
+                        () => subscribeToBox(subsls.id)
+                    }> Add {userDetails && userDetails.id} to Box {subsls.id}
+                    </button>
+            )
+        }
     </div>
 }
