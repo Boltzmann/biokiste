f2607990f5fa53916cea44590d7ffc06b683ca6e
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-06-07 00:13:11 +0200

    Add logout Add check for expiry of token Put details pages in authentication zone Login button now in active color

--- web-frontend/src/components/AllUserDetails.tsx
+++ web-frontend/src/components/AllUserDetails.tsx
@@ -1,15 +1,22 @@
 import {UserDetails} from "../model/UserDetails";
 import UserDetail from "./UserDetail";
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthProvider";
 
 type UserDetailsProps = {
     userDetails: UserDetails | undefined
 }
 export default function AllUserDetails({userDetails}: UserDetailsProps) {
+    const {logout} = useContext(AuthContext)
+
     return (
+        <div>
         <div id="inactive">
             {userDetails && Object.values(userDetails).map(
                 (value) => {return typeof value === 'string' ? <UserDetail detail={value}/> : <div/>}
             )}
+        </div>
+            <div id="active" onClick={() => logout()}>logout</div>
         </div>
     )
 }
