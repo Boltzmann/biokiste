d7cdf3d75b107ca96a479635a292714396538329
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-06-08 13:09:03 +0200

    Backend test with hard coded return value green. Changed test so that dto is send.

--- backend/src/main/java/com/github/boltzmann/biokiste/backend/controller/ItemController.java
+++ backend/src/main/java/com/github/boltzmann/biokiste/backend/controller/ItemController.java
@@ -1,12 +1,10 @@
 package com.github.boltzmann.biokiste.backend.controller;
 
+import com.github.boltzmann.biokiste.backend.dto.ItemDto;
 import com.github.boltzmann.biokiste.backend.model.Item;
 import com.github.boltzmann.biokiste.backend.service.ItemDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,10 @@ public class ItemController {
     @GetMapping(path="/all")
     public List<Item> getAllItems(){
         return itemDetailsService.getAllItems();
+    }
+
+    @PostMapping()
+    public Item createNewItem(@RequestBody ItemDto itemDto){
+        return Item.builder().id("1").name("one").build();
     }
 }
