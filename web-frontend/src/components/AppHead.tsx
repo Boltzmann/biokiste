ca36837d2b6c14888af37892f1112b61d0f24f76
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-06-13 16:46:20 +0200

    AppUser in Header Todo: CSS

--- web-frontend/src/components/AppHead.tsx
+++ web-frontend/src/components/AppHead.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./AppHead.css"
 import {useNavigate} from "react-router-dom";
+import ImportantUserDetails from "./ImportantUserDetails";
+import {UserDetails} from "../model/UserDetails";
 
-export default function AppHead(){
+type AppHeadProps = {
+    userDetails: UserDetails | undefined
+}
+
+export default function AppHead( {userDetails}: AppHeadProps){
     const navigate = useNavigate()
 
     return (
         <header>
+            <ImportantUserDetails userDetails={userDetails} />
             <button className="centered" id="active" onClick={() => navigate("/")}>Biokiste</button>
         </header>
     )
--- web-frontend/src/pages/OverviewPage.tsx
+++ web-frontend/src/pages/OverviewPage.tsx
@@ -1,24 +1,20 @@
-import ImportantUserDetails from "../components/ImportantUserDetails";
 import Abonnements from "../components/Abonnements";
 import {Subscription} from "../model/Subscription";
-import {UserDetails} from "../model/UserDetails";
 import {SubscriptionOverviewDto} from "../dto/SubscriptionOverviewDto";
 
 type OverviewPageProps= {
     subscribeToBox: (boxId: string) => void
     subscriptions: Subscription[] | undefined
-    userDetails: UserDetails | undefined
     subscribables: SubscriptionOverviewDto[] | undefined
     removeFromSubscriptionOnce: (boxId: string) => void | undefined
 }
 
 export default function OverviewPage(
-    {subscribeToBox, removeFromSubscriptionOnce, subscriptions, userDetails, subscribables}: OverviewPageProps
+    {subscribeToBox, removeFromSubscriptionOnce, subscriptions, subscribables}: OverviewPageProps
 ) {
 
     return (
         <div>
-            <ImportantUserDetails userDetails={userDetails} />
             <Abonnements
                 subscriptions={subscriptions}
                 subscribeToBox={subscribeToBox}
